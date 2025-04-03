75d57a6ffcd516f31f8a06403f0423327bbfe51e
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-03 16:22:22 +0200

    feat: move img logo in public

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,9 @@
-import logoDC from '../assets/img/dc-logo.png';
 import Menu from './Menu';
 
 export default function Header() {
     return (
         <header>
-            <img src={logoDC} alt="Logo DC comics" />
+            <img src="/dc-logo.png" alt="Logo DC comics" />
             <Menu />
         </header>
     );
