7c5e3b65a70cbfa3fe81ffc9994cc3a201dbc91d
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-03 15:41:24 +0200

    feat: add general structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Footer from './components/footer';
+import Header from './components/header';
+import Main from './components/main';
+
 function App() {
     return (
         <>
-
+            <Header />
+            <Main />
+            <Footer />
         </>
     );
 }
