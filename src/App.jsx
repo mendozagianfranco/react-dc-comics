098d2807fe38b9297d3f313f698250c801205b99
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-03 18:18:05 +0200

    feat: move icon image in public and create structure section icon links

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
+import IconLinks from './components/IconLinks';
 
 function App() {
     return (
         <>
             <Header />
             <Main />
+            <IconLinks />
             <Footer />
         </>
     );
---
+++ src/components/IconLinks.jsx
@@ -0,0 +1,11 @@
+export default function IconLinks() {
+    return (
+        <div className='icon-links'>
+            <div className="container">
+                <div className="card">
+                    <img src="/buy-comics-digital-comics.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
