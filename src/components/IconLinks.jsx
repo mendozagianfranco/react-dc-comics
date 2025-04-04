9b4e07ddc08aba9086643f10b7c7e332f86cd605
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-04 15:38:15 +0200

    feat: complete icon links

--- src/components/IconLinks.jsx
+++ src/components/IconLinks.jsx
@@ -1,8 +1,14 @@
+import Card from './Card';
+
 export default function IconLinks() {
     return (
         <div className='icon-links'>
-            <div className="container">
-                {/* <Card /> */}
+            <div className="container container-icon-links">
+                <Card image='/buy-comics-digital-comics.png' alt='digital comics' text='Digital Comics' />
+                <Card image='/buy-comics-merchandise.png' alt='Dc Merchandise' text='DC Merchandise' />
+                <Card image='/buy-comics-subscriptions.png' alt='Subscription' text='Subscription' />
+                <Card image='/buy-comics-shop-locator.png' alt='Comic Shop Locator' text='Comic Shop Locator' />
+                <Card image='/buy-dc-power-visa.svg' alt='DC power visa' text='DC Power Visa' />
             </div>
         </div>
     );
